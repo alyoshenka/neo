a56ce39cb37117ece5065f388155b60f8353c756
Alexi <38815390+alyoshenka@users.noreply.github.com>
2023-02-07 17:19:42 -0800

    Add aws submodule (#25)
    * move old aws connection files
    
    * more reorganization, plus adding more to pylint
    
    * Update pylint.yml
    
    * this works
    
    * add aws submodule
    
    * pylint works locally
    
    * relative import errors
    
    * init submodule in pylint workflow
    
    * requrements + install
    
    * requrements + install
    
    * remove unecessary requirements
    
    * update req versions
    
    * playing around with reqs
    
    * playing around with reqs
    
    * maybe it was a circular import?
    
    * maybe init was causing circular?
    
    * no more init
    
    * pylint already installed
    
    * sync pylint version
    
    * fix bad syntax
    
    * that didn't fix it
    
    * fixed linting errors I couldn't ignore

--- aws_iot/connection_builder.py
+++ aws_iot/connection_builder.py
@@ -1,35 +1,20 @@
-"""Subscribes to topic 'sub_topic', listens for 20s, then exits"""
+"""Handles building connections to AWS via MQTT protocol"""
 
 # pylint: disable=superfluous-parens
-# pylint: disable=unspecified-encoding
-# pylint: disable=import-error
-# pylint: disable=consider-using-f-string
-# pylint: disable=fixme
-# todo: fixme
 
 import json
-import time as t
-
 from awscrt import io, mqtt # , auth, http
 from awsiot import mqtt_connection_builder
 
-# todo: secrets with python
-CERTS_DIR = '/home/jay/Desktop/credentials/aws/iot/' # Where certificates are stored
-# ENDPOINT = None # AWS endpoint
-CLIENT_ID = 'Hubble' # Can be whatever
-CERT = CERTS_DIR + 'Hubble.cert.pem' # Device certificate
-KEY = CERTS_DIR + 'Hubble.private.key' # Private key
-ROOT_CA = CERTS_DIR + 'root-CA.crt' # todo: what is this?
 
-PUB_TOPIC = 'test' # Topic to publish to
-MESSAGE = 'hello!' # message to send
+from info import CERTS_DIR, CERT, KEY, ROOT_CA, CLIENT_ID
 
-SUB_TOPIC = "sub_topic" # topic to subscribe to
 
-def initialize_mqtt_connection():
+def create_mqtt_connection():
     """Initializes the connection to AWS"""
 
-    with open(CERTS_DIR + 'endpoint.txt', 'r') as endpoint_file:
+
+    with open(CERTS_DIR + 'endpoint.txt', 'r', encoding='utf-8') as endpoint_file:
         # pylint: disable=invalid-name
         ENDPOINT = endpoint_file.readline()
         endpoint_file.close()
@@ -49,9 +34,7 @@ def initialize_mqtt_connection():
         keep_alive_secs=6
         )
 
-
-    print("Connecting to {} with client ID '{}'...".format(
-        ENDPOINT, CLIENT_ID))
+    print(f'Connecting to {ENDPOINT} with client ID "{CLIENT_ID}"...')
     # Make the connect() call
     connect_future = mqtt_connection.connect()
     # Future.result() waits until a result is available
@@ -75,13 +58,13 @@ def on_resubscribe_complete():
 # not sure why this takes 2 args?
 def on_message_received(topic, payload):
     """Callback for when a message is received"""
-    print("Received message from topic '{}': {}".format(topic, payload))
+    print(f'Received message from topic "{topic}": {payload}')
 
 def publish(mqtt_connection, topic, message):
     """Publish a message to a topic"""
     # Publish message to server desired number of times.
     print('Begin Publish')
-    data = "{}".format(MESSAGE)
+    data = f'{message}'
     message = {"message" : data}
     mqtt_connection.publish(topic=topic, payload=json.dumps(message), qos=mqtt.QoS.AT_LEAST_ONCE)
     print("Published: '" + json.dumps(message) + "' to the topic: " + topic)
@@ -89,23 +72,17 @@ def publish(mqtt_connection, topic, message):
 
 def subscribe(mqtt_connection, topic):
     """Subscribe to a topic"""
-    print("Subscribing to topic '{}'...".format(SUB_TOPIC))
+    print(f'Subscribing to topic "{topic}"...')
     # pylint: disable=unused-variable
     subscribe_future, packet_id = mqtt_connection.subscribe(
         topic=topic,
         qos=mqtt.QoS.AT_LEAST_ONCE,
         callback=on_message_received)
     subscribe_result = subscribe_future.result()
-    print("Subscribed with {}".format(str(subscribe_result['qos'])))
+    print(f'Subscribed with: {(subscribe_result["qos"])}')
 
 def disconnect(mqtt_connection):
     """Disconnect"""
     disconnect_future = mqtt_connection.disconnect()
     disconnect_future.result()
     print("Disconnected")
-
-MQTT_CONNECTION = initialize_mqtt_connection()
-subscribe(MQTT_CONNECTION, SUB_TOPIC)
-publish(MQTT_CONNECTION, PUB_TOPIC, MESSAGE)
-t.sleep(20) # Keep the connection alive for 20s
-disconnect(MQTT_CONNECTION)
---
+++ aws_iot/main.py
@@ -0,0 +1,26 @@
+"""Subscribes to topic 'sub_topic', listens for 20s, then exits"""
+
+# pylint: disable=superfluous-parens
+# pylint: disable=import-error
+# pylint: disable=fixme
+# todo: fixme
+
+
+import time as t
+
+from connection_builder import create_mqtt_connection, subscribe, publish, disconnect
+
+
+
+PUB_TOPIC = 'test' # Topic to publish to
+MESSAGE = 'hello!' # message to send
+
+SUB_TOPIC = "sub_topic" # topic to subscribe to
+
+
+
+MQTT_CONNECTION = create_mqtt_connection()
+subscribe(MQTT_CONNECTION, SUB_TOPIC)
+publish(MQTT_CONNECTION, PUB_TOPIC, MESSAGE)
+t.sleep(20) # Keep the connection alive for 20s
+disconnect(MQTT_CONNECTION)
