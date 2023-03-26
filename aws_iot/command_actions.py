cbe1523bc0629eecfe1a66bafd4663719e10aec4
Jay <alexibmost@gmail.com>
2023-03-26 07:30:04 -0700

    add neopixeltest

--- aws_iot/command_actions.py
+++ aws_iot/command_actions.py
@@ -1,8 +1,21 @@
 """Capabilities of this program; what actions it can do"""
 
+import os
+
+import sys
+import subprocess
+
 def print_message(msg):
     """Print a message to the console"""
     print('print command:', msg)
+
+def run_neopixel_test():
+    """Runs a program that displays a simple animation on the LED board"""
+    # os.system('neopixeltest')
+    # subprocess.call('neopixeltest')
+    print('Recieved "neopixeltest" command')
+    sp = subprocess.Popen(["/bin/bash", "-i", "-c", 'neopixeltest'])
+    sp.communicate()
 
 # not sure why this takes 2 args?
 # todo: what action should this take?
