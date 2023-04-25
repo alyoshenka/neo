fab5206f3bdd3579bdf857ba63d1a9450b13292d
Alexi <alexibmost@gmail.com>
2023-04-25 12:06:02 -0700

    still not working

---
+++ neo/test_submodule.py
@@ -0,0 +1,15 @@
+
+import sys
+sys.path.append('neopolitan/neopolitan/')
+
+import neopolitan.neopolitan.const as neop_const
+
+print(neop_const.WIDTH)
+
+import neopolitan.neopolitan.writing.letters_8 as letters
+print(letters.a)
+
+"""
+import neopolitan.neopolitan.main.main as func
+func()
+"""
