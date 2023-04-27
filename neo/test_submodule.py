3d3b1dfa90d41e9c6c35b001d751830542deb402
Alexi <alexibmost@gmail.com>
2023-04-27 08:58:17 -0700

    not sure

--- neo/test_submodule.py
+++ neo/test_submodule.py
@@ -1,7 +1,12 @@
 
 import sys
-sys.path.append('neopolitan/neopolitan/')
+import os
+sys.path.append(os.getcwd() + 'neopolitan/neopolitan/')
 
+sys.path.append(os.getcwd() + 'neopolitan/')
+sys.path.append(os.getcwd() + 'neopolitan/neopolitan/writing/')
+
+"""
 import neopolitan.neopolitan.const as neop_const
 
 print(neop_const.WIDTH)
@@ -9,7 +14,14 @@ print(neop_const.WIDTH)
 import neopolitan.neopolitan.writing.letters_8 as letters
 print(letters.a)
 
+print(dir())
+print(sys.path)
+
+import os
+print(os.getcwd())
+
+import neopolitan.neopolitan.main as neop
+neop.main()
 """
-import neopolitan.neopolitan.main.main as func
-func()
-"""
+
+import neopolitan.neopolitan.writing.groups_8 as groups
