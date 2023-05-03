8645012f4fe4696447f885cc48bb92f3f0f0ecd1
Alexi <alexibmost@gmail.com>
2023-05-03 15:20:25 -0700

    import

--- tests/test_build_a_connection.py
+++ tests/test_build_a_connection.py
@@ -1,11 +1,13 @@
 """Tests building the MQTT connection"""
 
+import logging
 from neo.connection_builder import create_mqtt_connection, disconnect
 
 def test_build_connection():
     """Tests that an MQTT connection can be built without errors"""
     try:
         mqtt_connection = create_mqtt_connection()
+    # pylint: disable=broad-except
     except Exception as err:
         logging.error(err)
         assert False
