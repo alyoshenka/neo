826d50eac5be5865eea8bd5f66d04ff22d8e5f86
Alexi <38815390+alyoshenka@users.noreply.github.com>
2023-05-14 10:26:28 -0700

    change so that python 3.7 works (#86)
    * it works!
    
    * lint

--- neo/log.py
+++ neo/log.py
@@ -3,13 +3,19 @@
 import logging
 import datetime
 import os
+import sys
 
 def init_logger():
     """Set up the log file"""
     if not os.path.exists('logs/'):
         os.makedirs('logs/')
     filename = 'logs/neo ' + str(datetime.datetime.now()) + '.txt'
-    logging.basicConfig(filename=filename, encoding='utf=8', level=logging.DEBUG)
+    if sys.version_info[:2] > (3, 7):
+        logging.basicConfig(filename=filename, encoding='utf=8', level=logging.DEBUG)
+    else:
+        logging.basicConfig(filename=filename, level=logging.DEBUG)
+
+    get_logger().info('Running with Python %s.%s', sys.version_info[0], sys.version_info[1])
 
 def get_logger():
     """Get the logger for this application"""
